c5145c470fff6dab3fbcbb002c7cdf06a2f40db3
Riki <zccz14@outlook.com>
2016-04-23 16:11:31 +0800

    Update

--- export.sh
+++ export.sh
@@ -1,2 +1,5 @@
 mongoexport --db spider --collection airs --out airs.csv --type csv --fields date,location,aqi,pm25
 mongoexport --db spider --collection weathers --out weathers.csv --type csv --fields date,location,temp,humi,rain,windd,windv,pressure,visi
+git add *
+git commit -m "Update"
+git push
