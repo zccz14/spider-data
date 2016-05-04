fb1269f3a8cae20922b244c1bd19c89d3bb76b1e
Riki <zccz14@outlook.com>
2016-05-04 19:50:52 +0800

    Update

--- export.sh
+++ export.sh
@@ -1,5 +1,6 @@
 mongoexport --db spider --collection airs --out airs.csv --type csv --fields date,location,aqi,pm25
 mongoexport --db spider --collection weathers --out weathers.csv --type csv --fields date,location,temp,humi,rain,windd,windv,pressure,visi
+mongoexport --db spider --collection roads --out roads.csv --type csv --fields date,name,direction,traffic,speed,level
 git add *
 git commit -m "Update"
 git push
